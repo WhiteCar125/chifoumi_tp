527255e5c357bf7f12648b95f937965d44f19a04
Mael <lemenn-m08@saint-louis29.net>
2024-10-09 09:25:51 +0200

    V1

--- tp_chifoumi.py
+++ tp_chifoumi.py
@@ -1,5 +1,8 @@
 import random
 
 def choix_ordi():
-    mylist = ['p','f','c']    
-    print(random.choice(mylist))
+    c = random.choices('p','f','c')
+    return c
+
+def choix_joueur():
+    choix = str(input("Choisissez p, f, ou,c"))
