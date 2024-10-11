eac4d8a5e7538b8053daa6d05c64269305fb5cf0
Mael <lemenn-m08@saint-louis29.net>
2024-10-11 13:50:57 +0200

    fin

--- tp_chifoumi.py
+++ tp_chifoumi.py
@@ -1,8 +1,34 @@
 import random
+while True:
+    def choix_ordi():
+     c = random.choices(str('p f c'))
+     return c
 
-def choix_ordi():
-    c = random.choices('p','f','c')
-    return c
+    def choix_joueur():
+        choix = str(input("Choisissez p, f, ou, c\n>>"))
+        return choix
 
-def choix_joueur():
-    choix = str(input("Choisissez p, f, ou,c"))
+    def verdict(choix_joueur,choix_ordi):
+        if choix_joueur == choix_ordi:
+            return 0
+
+        if choix_joueur=='p' and choix_ordi == 'c' or choix_joueur=='f' and choix_ordi=='p' or choix_joueur=='c' and choix_ordi=='f':
+            return 1
+
+        else :
+            return -1
+
+    def partie():
+        joueur = choix_joueur() 
+        ordi = choix_ordi()
+        print(ordi)
+        return verdict(joueur,ordi)
+
+    def jeu_pfc():
+        s = 0
+        verdict = partie()
+        s = s + verdict
+        print(s)
+
+    jeu_pfc()
+
